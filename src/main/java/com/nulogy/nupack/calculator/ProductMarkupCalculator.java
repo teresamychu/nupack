2330e6b0141f4b677812c6b994202fa754973ef2
Teresa Chu <dev+teresa@influitive.com>
2017-01-15 00:17:56 -0500

    Initial working calculator. Calculator should return final cost in currency format.

--- src/main/java/com/nulogy/nupack/calculator/ProductMarkupCalculator.java
+++ src/main/java/com/nulogy/nupack/calculator/ProductMarkupCalculator.java
@@ -1,8 +1,10 @@
 package com.nulogy.nupack.calculator;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 import com.nulogy.nupack.product.Product;
+import com.nulogy.nupack.product.ProductType;
 
 public class ProductMarkupCalculator {
     
@@ -10,7 +12,38 @@ public class ProductMarkupCalculator {
         
     }
 
-    public BigDecimal calculate(Product product) {
-        return null;
-    }  
+    public String calculate(Product product) {
+        BigDecimal flatMarkupValue = product.getPrice().multiply(new BigDecimal(1.05));
+        BigDecimal productTypeMarkup = calculateProductTypeMarkup(flatMarkupValue, product.getProductType());
+        BigDecimal peopleMarkup = calculatePeopleMarkup(flatMarkupValue, product.getNumOfPeople());
+        BigDecimal finalCost =  flatMarkupValue.add(productTypeMarkup).add(peopleMarkup);
+        return NumberFormat.getCurrencyInstance().format(finalCost);
+        
+    }
+    
+    private BigDecimal calculateProductTypeMarkup(BigDecimal flatMarkupValue, ProductType productType) {
+        BigDecimal productTypeMarkup = new BigDecimal(0);
+        switch (productType) {
+            case PHARMACEUTICALS:
+                productTypeMarkup = flatMarkupValue.multiply(new BigDecimal(0.075));
+                break;
+            case ELECTRONICS:
+                productTypeMarkup = flatMarkupValue.multiply(new BigDecimal(0.02));
+                break;
+            case FOOD:    
+                productTypeMarkup = flatMarkupValue.multiply(new BigDecimal(0.13));
+                break;
+            default:
+                break;
+        }
+        return productTypeMarkup;
+    }
+    
+    private BigDecimal calculatePeopleMarkup(BigDecimal flatMarkupValue, int numOfPeople) {
+        double markupPercent = numOfPeople*0.012;
+        return flatMarkupValue.multiply(new BigDecimal(markupPercent));
+    }
+    
+    
+    
 }
--- src/test/java/com/nulogy/nupack/calculator/ProductMarkupCalculatorTest.java
+++ src/test/java/com/nulogy/nupack/calculator/ProductMarkupCalculatorTest.java
@@ -1,6 +1,6 @@
 package com.nulogy.nupack.calculator;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
@@ -19,64 +19,52 @@ public class ProductMarkupCalculatorTest {
        this.calc = new ProductMarkupCalculator();
    }
    
-   public void tearDown() {
-       
-   }
-   
    @Test
    public void testCalculateFlatMarkupOnly() {
        Product product = new Product(new BigDecimal(1.00), ProductType.OTHER, 0);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(1.05)));
+       assertEquals(this.calc.calculate(product), "$1.05");
    }
    
    @Test
    public void testCalculateProductTypeFoodOnly() {
        Product product = new Product(new BigDecimal(1.00), ProductType.FOOD, 0);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(1.1865)));
+       assertEquals(this.calc.calculate(product), "$1.19");
    }
    
    @Test
    public void testCalculateProductTypeElectronicsOnly() {
        Product product = new Product(new BigDecimal(1.00), ProductType.ELECTRONICS, 0);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(1.071)));
+       assertEquals(this.calc.calculate(product), "$1.07");
    }
    
    @Test
    public void testCalculatePeopleMarkupOnly() {
        Product product = new Product(new BigDecimal(1.00), ProductType.OTHER, 1);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(1.0626)));
+       assertEquals(this.calc.calculate(product), "$1.06");
    }
    
    @Test
    public void testCalculateProductTypePharmaceuticalsOnly() {
        Product product = new Product(new BigDecimal(1.00), ProductType.PHARMACEUTICALS, 0);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(1.12875)));
+       assertEquals(this.calc.calculate(product), "$1.13");
    }
    
    @Test
    public void testCalculateExample1() {
        Product product = new Product(new BigDecimal(1299.99), ProductType.FOOD, 3);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(1591.58)));
+       assertEquals(this.calc.calculate(product), "$1,591.58");
    }
    
    @Test
    public void testCalculateExample2() {
-       Product product = new Product(new BigDecimal(5432.00), ProductType.PHARMACEUTICALS, 1);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(6199.81)));
+       Product product = new Product(new BigDecimal(5432.00), ProductType.PHARMACEUTICALS, 1);      
+       assertEquals(this.calc.calculate(product), "$6,199.81");
    }
    
    @Test
    public void testCalculateExample3() {
        Product product = new Product(new BigDecimal(12456.95), ProductType.OTHER, 4);
-       BigDecimal finalCost = this.calc.calculate(product);
-       assertTrue(finalCost.equals(new BigDecimal(13707.63)));
+       assertEquals(this.calc.calculate(product), "$13,707.63");
    }
    
 }
