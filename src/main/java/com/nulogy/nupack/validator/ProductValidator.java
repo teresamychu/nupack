e4a964897f1fba8edf3c93fc1515e5140a0ecb56
Teresa Chu <dev+teresa@influitive.com>
2017-01-16 21:40:26 -0500

    Validator should validate product price/numofpeople/product type.

--- src/main/java/com/nulogy/nupack/validator/ProductValidator.java
+++ src/main/java/com/nulogy/nupack/validator/ProductValidator.java
@@ -1,7 +1,10 @@
 package com.nulogy.nupack.validator;
 
+import java.math.BigDecimal;
+
 import com.nulogy.nupack.exception.ProductValidationException;
 import com.nulogy.nupack.product.Product;
+import com.nulogy.nupack.product.ProductType;
 
 public class ProductValidator {
     
@@ -9,8 +12,31 @@ public class ProductValidator {
     }
     
     public boolean validate(Product product) throws ProductValidationException {
-     return false;
+     return validateProductPrice(product.getPrice())
+            && validateProductNumOfPeople(product.getNumOfPeople())
+            && validateProductType(product.getProductType());
+             
     }
     
+    private boolean validateProductPrice(BigDecimal productPrice) throws ProductValidationException {
+        if (productPrice == null || productPrice.compareTo(new BigDecimal(0) ) == -1) {
+            throw new ProductValidationException("Invalid Product.price value: " + productPrice);
+        } 
+        return true;
+    }
+    
+    private boolean validateProductNumOfPeople(int numOfPeople) throws ProductValidationException {
+        if ( numOfPeople < 0) {
+           throw new ProductValidationException("Invalid Product.numOfPeople value: " + numOfPeople);
+        }
+        return true;
+    }
+    
+    private boolean validateProductType(ProductType type) throws ProductValidationException {
+        if (type == null) {
+            throw new ProductValidationException("Invalid Product.productType value: " + type);
+        }
+        return true;
+    }
     
 }
